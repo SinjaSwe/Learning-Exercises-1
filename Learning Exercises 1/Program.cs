c3a5f3c35c37457f0f52c8ed1e912d39c6c409ab
Sinead Jacobsson <sinead.jacobsson@gmail.com>
2020-09-11 08:40:35 +0200

    Add project files.

---
+++ Learning Exercises 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Learning_Exercises_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMethod();
+
+            static void MyMethod()
+            {
+                string firstname;
+                string surname;
+
+                Console.WriteLine("What is your firstname?");
+                firstname = Console.ReadLine();
+
+                Console.WriteLine("What is your surname?");
+                surname = Console.ReadLine();
+
+                Console.WriteLine("Hello " + firstname + " " +surname + "! I'm glad to inform you that you are the test subject of my very first assignment!");
+                Console.ReadLine();
+
+
+            }
+
+
+            
+
+            {
+
+                
+
+                
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+    } }
