b076ec25b8f2859f13ae99c629e995528a508d16
Sinead Jacobsson <sinead.jacobsson@gmail.com>
2020-09-11 10:57:04 +0200

    RunExercise3

--- Learning Exercises 1/Program.cs
+++ Learning Exercises 1/Program.cs
@@ -1,57 +1,132 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Transactions;
 
 namespace Learning_Exercises_1
 {
     class Program
     {
+        private static void RunExerciseOne()
+        {
+            Console.WriteLine("You successfully an exercise one!");
+        }
+
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You sucessfully ran exercise two!");
+        }
+
         static void Main(string[] args)
         {
-            RunExerciseOne();
 
-            static void RunExerciseOne()
+            var keepAlive = true;
+            while (keepAlive)
             {
-                string firstname;
-                string surname;
+                try
+                {
+                    Console.WriteLine("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case 3:
+                            RunExerciseThree();
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This is not a valid assignment number!");
+                            break;
+                    }
 
-                Console.WriteLine("What is your firstname?");
-                firstname = Console.ReadLine();
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
 
-                Console.WriteLine("What is your surname?");
-                surname = Console.ReadLine();
-
-                Console.WriteLine("Hello " + firstname + " " +surname + "! I'm glad to inform you that you are the test subject of my very first assignment!");
-                Console.ReadLine();
-
-
-            }
-
-            RunExerciseTwo();
-
-            static void RunExerciseTwo()
-            {
-
-                //Today's date
-                Console.WriteLine("Today's date is: " + DateTime.Now.ToString("d/MM/yyyy"));
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid assignment number!");
+                    Console.ResetColor();
+                }
 
 
+                RunExerciseOne();
 
-                //Tomorrow's date
-                Console.WriteLine("Tomorrow's date is: " + DateTime.Now.AddDays(1).ToString("d/MM/yyyy"));
+                static void RunExerciseOne()
+                {
+                    string firstname;
+                    string surname;
 
-                // Yesterday's date
-                Console.WriteLine("Yesterday's date was: " + DateTime.Now.AddDays(-1).ToString("d/MM/yyyy"));
+                    Console.WriteLine("What is your firstname?");
+                    firstname = Console.ReadLine();
+
+                    Console.WriteLine("What is your surname?");
+                    surname = Console.ReadLine();
+
+                    Console.WriteLine("Hello " + firstname + " " + surname + "! I'm glad to inform you that you are the test subject of my very first assignment!");
+                    Console.ReadLine();
 
 
-            }
+                }
+
+                RunExerciseTwo();
+
+                static void RunExerciseTwo()
+                {
+
+                    //Today's date
+                    Console.WriteLine("Today's date is: " + DateTime.Now.ToString("d/MM/yyyy"));
+
+                    //Tomorrow's date
+                    Console.WriteLine("Tomorrow's date is: " + DateTime.Now.AddDays(1).ToString("d/MM/yyyy"));
+
+                    // Yesterday's date
+                    Console.WriteLine("Yesterday's date was: " + DateTime.Now.AddDays(-1).ToString("d/MM/yyyy"));
 
 
-            
+                }
 
-            {
 
+                RunExerciseThree();
+
+                static void RunExerciseThree()
+
+                {
+                    string firstname;
+                    string lastname;
+
+                    Console.Write("Enter your firstname:");
+                    firstname = Console.ReadLine();
+
+                    Console.Write("Enter your lastname:");
+                    lastname = Console.ReadLine();
+
+                    var firstnameNew = firstname;
+                    var lastnameNew = lastname;
+
+                    Console.Write(firstnameNew + " " + lastnameNew);
+                    Console.ReadLine();
+                }
+
+                RunExerciseFour();
                 
+                static void RunExerciseFour()
 
-                
+                {
+                    String str = "The quick fox Jumped Over the DOG";
+
+                }
 
 
 
@@ -59,10 +134,8 @@ namespace Learning_Exercises_1
 
 
             }
-
-
-
-
-
         }
-    } }
+
+    }
+
+}
